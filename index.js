7ff50d0e7f27e6178f1d2c2f344b84e7fd3c35d5
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-08-16 00:47:35 +0600

    add:pagination

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app=express();
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -11,12 +11,60 @@ app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fbvkkp8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const jobtaskphcollection = client.db('jobPh').collection('products');
+
+    app.get('/products', async (req, res) => {
+      const page = parseInt(req.query.page) || 1; 
+      const limit = parseInt(req.query.limit) || 10; 
+      const skip = (page - 1) * limit;
+  
+      const cursor = jobtaskphcollection.find().skip(skip).limit(limit);
+      const result = await cursor.toArray();
+  
+      // Get the total count of products
+      const totalProducts = await jobtaskphcollection.countDocuments();
+  
+      res.send({
+          products: result,
+          currentPage: page,
+          totalPages: Math.ceil(totalProducts / limit),
+          totalProducts
+      });
+  });
+  
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 
 app.get('/',(req,res)=>{
-    res.send('the  Tourists Spot server is running')
+    res.send('the  product server is running')
 })
 
 app.listen(port,()=>{
-    console.log(`the Tourists Spot is running port : ${port}`)
+    console.log(`the product is running port : ${port}`)
 })
